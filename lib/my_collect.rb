372f646d23838b8ea36ea1f0a6d55c8dbb1516ea
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:36:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,15 @@
 
 def my_collect(array)
     i = 0
+    narray = []
   if array.size == 0
     return nil
   else
     while i < array.size
       yield(array[i])
+      narray << array[i]
       i += 1
     end
   end
-  array
+  narray
 end
