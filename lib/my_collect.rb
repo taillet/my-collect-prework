ee4cb24e5c4e127cc3a7b8790d81c686cafbde35
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:40:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
-
+def my_collect(array)
+    i = 0
+    narray = []
+  if array.size == 0
+    return nil
+  else
+    while i < array.size
+      narray << yield(array[i])
+      i += 1
+    end
+    narray
+  end
+end
